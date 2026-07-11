c209d4d0f3de12c68975ce0fb7f6e97b74dcba16
Dan Corin <dcorin6@gmail.com>
2026-07-10 21:46:44 -0400

    Initial Commit

---
+++ Reco/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reco
+//
+//  Created by Daniel Corin on 7/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reco/RecoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecoApp.swift
+//  Reco
+//
+//  Created by Daniel Corin on 7/10/26.
+//
+
+import SwiftUI
+
+@main
+struct RecoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
